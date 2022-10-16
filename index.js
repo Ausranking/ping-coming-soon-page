e825024592287a71a25560f26304221114762c75
Austin Nze <austinnze1000@gmail.com>
2022-10-16 12:37:01 +0100

    Updated project

--- index.js
+++ index.js
@@ -1,31 +1,28 @@
-// function validateEmail() {
-// let email = document.getElementById("email")
-//   let x = document.forms["action-form"]
-
-//   ["email"].value
-
-//   if (x == "") {
-//     alert("invalid email");
-//     email.style.border ="1px solid red"
-//     return false;
-//   }
-// }
-
 const form = document.forms[0];
 const small = document.getElementById("small");
-const email = form.elements["email"]
+const email = form.elements["email"];
 
 validateEmail = (e) => {
-const validEmail = email.value;
-if (validEmail.trim()===""){
-  email.style.border = "1px solid red"
-  small.innerHTML =`<i><h5>please provide a valid email address<h5></i>`
-  small.style.color ="red"
-  small.style.fontSize ="14px"
-  
-  return false
-}else{
-  alert(`you have suscribed and we will keep you notified.`)
-  return true
-}
+  const validEmail = email.value;
+  if (validEmail.trim() === "") {
+    email.style.border = "1px solid red";
+    small.innerHTML = `<i><h5>please provide a valid email address<h5></i>`;
+    small.style.color = "red";
+    small.style.fontSize = "14px";
+
+    return false;
+  } else {
+    alert(`you have suscribed and we will keep you notified.`);
+    return true;
+  }
 };
+
+// let number = prompt(`enter number to continue`)
+// for (let number = 0; number < 9; number += 2) {
+//   console.log(number);
+// }
+
+// let result = 1
+// for(counter = 0 ; counter < 10 ; counter++) {
+//   result *= 2;
+// }console.log (result)
