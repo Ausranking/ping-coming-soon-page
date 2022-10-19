b21537cc3e3b01a8321792a3019ab0898f06306f
Austin Nze <austinnze1000@gmail.com>
2022-10-19 01:15:03 +0100

    update all

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ validateEmail = (e) => {
   const validEmail = email.value;
   if (validEmail.trim() === "") {
     email.style.border = "1px solid red";
-    small.innerHTML = `<i><h5>please provide a valid email address<h5></i>`;
+    small.innerHTML = `<i><h5>email address cannot be empty<h5></i>`;
     small.style.color = "red";
     small.style.fontSize = "14px";
 
@@ -15,7 +15,7 @@ validateEmail = (e) => {
     alert(`you have suscribed and we will keep you notified.`);
     return true;
   }
-};
+}
 
 // let number = prompt(`enter number to continue`)
 // for (let number = 0; number < 9; number += 2) {
@@ -25,4 +25,3 @@ validateEmail = (e) => {
 // let result = 1
 // for(counter = 0 ; counter < 10 ; counter++) {
 //   result *= 2;
-// }console.log (result)
